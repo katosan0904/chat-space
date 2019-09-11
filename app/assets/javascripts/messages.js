ebb681992fe41fde9c68682d7ab466658f235f85
katosan0904 <gachikiritanpo@gmail.com>
2019-09-11 19:24:08 +0900

    first commit

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,23 +1,28 @@
 $(function(){
-  function buildHTML(message) {
-    var html = `<div class='message'>
-    <div class='upper-message'>
-    <div class='upper-message__user-name'>
-    ${message.user_name}
-    </div>
-    <div class='upper-message__date'>
-    ${message.date}
-    </div>
-    </div>
-    <div class='lower-message'>
-    <p class='lower-message__content'>
-    ${message.content}
-    </p>
-    ${message.image}
-    </div>
-    </div>`
-  return html;
-  }
+  if (document.URL.match("/messages")) {
+    function buildHTML(message){
+    var content= message.content? message.content :""
+    var image= message.image? `<img src=${message.image.url}  class="lower-message__image" ></img>` :`<div class="no-image"></div>`
+
+      var html = `<div class="message" data-id='${message.id}'>
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name}
+          </div>
+          <div class="upper-message__date">
+            ${message.date}
+          </div>
+        </div>
+        <div class="lower-message">
+        <p class='lower-message__content' data-id=${message.id}>
+            ${content}
+          </p>
+          ${image}
+        </div>
+      </div>`
+      
+    return html;
+  };
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var message = new FormData(this);
@@ -30,6 +35,7 @@ $(function(){
       processData: false,
       contentType:false
     })
+
     .done(function(message){
       var html = buildHTML(message);
       $('.messages').append(html);
@@ -46,5 +52,39 @@ $(function(){
     .always(function(data){
       $("input[type='submit']").attr('disabled', false);
   })
+
+
   })
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+   var id = $('.lower-message__content:last').data('id');
+   last_message_id = id
+   
+      $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: 'api/messages' ,
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: "GET",
+      dataType: "json",
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    }) 
+    .done(function(messages) {
+      var insertHTML = '';
+       (messages).forEach(function(message){
+      var html=buildHTML(message)
+      $('.messages').append(html)
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
+     
+      })
+      
+    })
+    
+    .fail(function() {
+      console.log('error');
+    });
+  }
+  setInterval(reloadMessages, 5000);
+};
 });
